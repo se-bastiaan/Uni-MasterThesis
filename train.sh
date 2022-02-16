a13ecb0bfbdf76a54285e4ae3fafbde8b4eb9059
Sébastiaan Versteeg <sversteeg@cn99.science.ru.nl>
2022-02-16 20:57:43 +0100

    Add slurm sh script

---
+++ train.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+#SBATCH -J inpainting-transformer
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sversteeg@science.ru.nl
+#SBATCH --gres=gpu:4i
+#SBATCH --partition=csedu
+#SBATCH --mem=32G
+#SBATCH -N 1 -n 4
+#
+
+source /ceph/csedu-scratch/project/sversteeg/venv/bin/activate
+python main.py --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE}
