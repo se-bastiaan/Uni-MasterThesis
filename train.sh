53ac07d978c26ae157df24754823f74d34b3476f
Sébastiaan <se_bastiaan@outlook.com>
2022-02-18 12:03:25 +0100

    Adjust train.sh

--- train.sh
+++ train.sh
@@ -6,9 +6,10 @@
 #SBATCH --mail-user=sversteeg@science.ru.nl
 #SBATCH --gres=gpu:4
 #SBATCH --partition=csedu
-#SBATCH --mem=32G
-#SBATCH -N 1 -n 4
+#SBATCH --mem=16G
+#SBATCH -N 1 -n 1
 #
 
 source /ceph/csedu-scratch/project/sversteeg/venv/bin/activate
+which python
 python main.py --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE}
