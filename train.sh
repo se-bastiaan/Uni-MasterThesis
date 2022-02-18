4cbc2dcd3af6f6d229de7d9018ea40dc425f17c7
Sébastiaan <se_bastiaan@outlook.com>
2022-02-18 16:50:24 +0100

    Better train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 #
+#SBATCH -e slurm-%j-${IMAGE_TYPE}-${EPOCHS}-${ATTENTION_TYPE}.out
+#SBATCH -o slurm-%j-${IMAGE_TYPE}-${EPOCHS}-${ATTENTION_TYPE}.out
 #SBATCH -J inpainting-transformer
-#SBATCH --time=2-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sversteeg@science.ru.nl
 #SBATCH --gres=gpu:4
@@ -12,4 +14,7 @@
 
 source /ceph/csedu-scratch/project/sversteeg/venv/bin/activate
 which python
-python main.py --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE}
+echo "Image type: ${IMAGE_TYPE}"
+echo "Max epochs: ${EPOCHS}"
+echo "Attention type: ${ATTENTION_TYPE}"
+python main.py --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE} --dataset /ceph/csedu-scratch/project/sversteeg/mvtec-ad/ --output__path /ceph/csedu-scratch/project/sversteeg/output
