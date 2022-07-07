e5c494415684fa2fb662d1ca0ceaa2a84f2a829f
Sébastiaan <se_bastiaan@outlook.com>
2022-07-07 13:56:36 +0200

    Fix resume script

--- resume.sh
+++ resume.sh
@@ -15,4 +15,5 @@ which python
 echo "Image type: ${IMAGE_TYPE}"
 echo "Max epochs: ${EPOCHS}"
 echo "Attention type: ${ATTENTION_TYPE}"
-python main.py --resume_checkpoint ${RESUME} --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE} --dataset /ceph/csedu-scratch/project/sversteeg/mvtec-ad/ --output_path /ceph/csedu-scratch/project/sversteeg/output
+echo "Checkpoint version: ${CHECKPOINT}"
+python main.py --resume_checkpoint ${CHECKPOINT} --image_type ${IMAGE_TYPE} --max_epochs ${EPOCHS} --attention_type ${ATTENTION_TYPE} --dataset /ceph/csedu-scratch/project/sversteeg/mvtec-ad/ --output_path /ceph/csedu-scratch/project/sversteeg/output
